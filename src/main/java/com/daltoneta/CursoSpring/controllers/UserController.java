cadee59e12c77af4d02e743e1e8e607b92c56370
LOctavioDev <octaviodevtech@gmail.com>
2023-12-17 23:13:03 -0600

    Hello Word

---
+++ src/main/java/com/daltoneta/CursoSpring/controllers/UserController.java
@@ -0,0 +1,16 @@
+package com.daltoneta.CursoSpring.controllers;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("user")
+
+public class UserController {
+    
+    @RequestMapping(value = "/holamundo", method = RequestMethod.GET)
+    String holaMundo() {
+        return "Hola Mundo";
+    }
+}
